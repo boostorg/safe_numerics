ecabaaaa766297a611f718791260f83a2081ae8d
Robert Ramey <ramey@rrsd.com>
2016-11-07 14:47:40 -0800

    updated sample for to illustrate safe numerics role in addressing a classic C++ problem

--- examples/example4.cpp
+++ examples/example4.cpp
@@ -3,33 +3,42 @@
 
 #include "../include/safe_integer.hpp"
 
-int main(int argc, const char * argv[]){
+int main(){
     std::cout << "example 4: ";
     std::cout << "implicit conversions change data values" << std::endl;
     std::cout << "Not using safe numerics" << std::endl;
+    
     // problem: implicit conversions change data values
     try{
-        int x = -1000;
-        // the following silently produces an incorrect result
-        char y = x;
-        std::cout << x << " != " << (int)y << std::endl;
-        std::cout << "error NOT detected!" << std::endl;
+        signed int   a{-1};
+        unsigned int b{1};
+        if(a < b){
+            std::cout << "a is less than b\n";
+        }
+        else{
+            std::cout << "b is less than a\n";
+        }
     }
     catch(std::exception){
-        std::cout << "error detected!" << std::endl; // never arrive here
+        // never arrive here - just produce the wrong answer!
+        std::cout << "error detected!" << std::endl;
     }
+
     // solution: replace int with safe<int> and char with safe<char>
     std::cout << "Using safe numerics" << std::endl;
     try{
         using namespace boost::numeric;
-        safe<int> x = -1000;
-        // throws exception when conversion change data value
-        safe<char> y1(x);
-        safe<char> y2 = x;
-        safe<char> y3 = {x};
-        std::cout << "error NOT detected!" << std::endl;
+        safe<signed int>   a{-1};
+        safe<unsigned int> b{1};
+        if(a < b){
+            std::cout << "a is less than b\n";
+        }
+        else{
+            std::cout << "b is less than a\n";
+        }
     }
     catch(std::exception & e){
+        // never arrive here - just produce the correct answer!
         std::cout << e.what() << std::endl;
         std::cout << "error detected!" << std::endl;
     }
