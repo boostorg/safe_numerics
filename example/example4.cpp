ec2eb0fdcb2feda2224d972b7a1c8ef0599c95b5
Giovanni Mascellani <g.mascellani@gmail.com>
2020-10-14 14:13:36 +0200

    Fix typo in copyright headers. (#85)

--- example/example4.cpp
+++ example/example4.cpp
@@ -1,4 +1,4 @@
-//  Copyright (c) 2018Robert Ramey
+//  Copyright (c) 2018 Robert Ramey
 //
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
--- test/test_subtract_native_constexpr.cpp
+++ test/test_subtract_native_constexpr.cpp
@@ -1,4 +1,4 @@
-//  Copyright (c) 2019Robert Ramey
+//  Copyright (c) 2019 Robert Ramey
 //
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
